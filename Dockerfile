44a65704dfea86755c5812ad42722227e360574a
Darkatse <39111369+Darkatse@users.noreply.github.com>
2020-11-02 11:26:09 +0800

    bugs fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
-FROM python:3.7-alpine
+FROM ubuntu:20.04
 MAINTAINER Darkatse <x.yngtze.river@gmail.com>
+
+ENV DEBIAN_FRONTEND="noninteractive"
+RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections; \
+    sed -Ei 's/security.ubuntu.com/mirrors.163.com/g' /etc/apt/sources.list; \
+    sed -Ei 's/archive.ubuntu.com/mirrors.163.com/g' /etc/apt/sources.list;
+
+
+RUN date && apt update && apt install -y cron openssl coreutils git wget tzdata \
+         && apt update && apt install -y python3 python3-pip 
 
 WORKDIR /
 
--- sync.sh
+++ sync.sh
@@ -10,31 +10,11 @@ git clone --depth=1 https://github.com/Zero-S1/xmly_speed.git /xmly_speed_tmp
   rm -rf /xmly_speed
   mv /xmly_speed_tmp /xmly_speed
 }
-cd /scripts || exit 1
-pip install --no-cache-dir -r requirements.txt || exit 1
+cd /xmly_speed || exit 1
+pip3 install --no-cache-dir -r requirements.txt || exit 1
 cp /crontab.list /crontab.list.old
 echo "55 */3 * * * bash /sync 1>/proc/1/fd/1 2>/proc/1/fd/2" > /crontab.list
-for file in $(find /xmly_speed/.github/workflows -type f);do
-  cat $file | grep -q 'cron:' && {
-    cat $file | grep -q 'python3 xmly_.*\.py' && {
-      a=$(cat $file | sed -En "s/^.*cron: '(.*)'.*$/\1/p")
-      b=$(cat $file | sed -En 's|^.*python3 (xmly_.*\.py)$|python3 /xmly_speed/\1|p')
-      name=$(cat $file | sed -En "s|^.*name: . run (.*)|\1|p" | sed "s/'//g")
-      echo $file
-      script="\"
-        exec 1<>/proc/1/fd/1;
-        exec 2<>/proc/1/fd/2;
-        set -o allexport;
-        source /all;
-        source /env;
-        cd /scripts;
-        $b | sed 's/^/$name/';
-      \""
-      script="$(echo ${script})"
-      echo "$a" bash -c "$script" >> /crontab.list
-    }
-  }
-done
+echo "*/30 * * * * set -o allexport; source /all; source /env; python3 /xmly_speed/xmly_speed.py 1>/proc/1/fd/1 2>/proc/1/fd/2" >> /crontab.list
 crontab -r
 crontab /crontab.list || {
   cp /crontab.list.old /crontab.list
